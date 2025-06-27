9300c5493cf2d7001079c5b0195b71038a13d808
Niiaks <napappoe007@st.ug.edu.gh>
2025-06-27 20:32:33 +0000

    set up expo font

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,6 +1,32 @@
 import { Text, View } from "react-native";
+import { useFonts } from "expo-font"
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
+// Prevent the splash screen from auto-hiding
+SplashScreen.preventAutoHideAsync()
 export default function Index() {
+  const [fontLoaded] = useFonts({
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      if (fontLoaded) {
+        await SplashScreen.hideAsync();
+      }
+    }
+    prepare();
+  }, [fontLoaded]);
+
+  if (!fontLoaded) {
+    return null;
+  }
   return (
     <View
       style={{
